2a725414f02786b7f57792bd86ef7d56ab4834f8
SafdarAliGit <safdar211@gmail.com>
2025-11-08 09:32:09 +0500

    updated

---
+++ mpkhi/public/js/sales_invoice.js
@@ -0,0 +1,16 @@
+frappe.ui.form.on('Sales Invoice Item', {    
+    custom_qty_in_ctn: function(frm, cdt, cdn) {
+        calculate_qty(cdt, cdn);
+    },
+    custom_no_of_ctn: function(frm, cdt, cdn) {
+        calculate_qty(cdt, cdn);
+    }
+});
+
+function calculate_qty(cdt, cdn) {
+    let row = locals[cdt][cdn];
+    let qty_in_ctn = flt(row.custom_qty_in_ctn);
+    let no_of_ctn = flt(row.custom_no_of_ctn);
+    let total_qty = qty_in_ctn * no_of_ctn;
+    frappe.model.set_value(cdt, cdn, 'qty', total_qty);
+}
